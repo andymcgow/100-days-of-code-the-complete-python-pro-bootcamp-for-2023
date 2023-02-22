1c47b3d6492c1bd67f992db92b7902893e5128ae
andymcgow <118489282+andymcgow@users.noreply.github.com>
2023-02-22 23:25:27 +0000

    Add files via upload

---
+++ 025-us-states-game/main.py
@@ -0,0 +1,42 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.setup(width=725, height=491)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# # Get state coordinates on image: -
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+missing_states = []
+for state in all_states:
+    if state not in guessed_states:
+        missing_states.append(state)
+
+series = pandas.Series(missing_states)
+series.to_csv("states_to_learn.csv")
